074053c5c3203858de6df0f3073213c9bfcf1843
zhangrui137 <zhangrui137@ke.com>
2022-04-06 15:51:28 +0800

    修复启动问题

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
-	"github.com/kataras/iris/mvc"
-	"github.com/kataras/iris/v12/_examples/mvc/overview/web/controllers"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
 )
 
 func main() {
 	app := iris.New()
 	app.Logger().SetLevel("debug")
-	app.Get("/", func(ctx *context.Context) {})
+	app.Get("/", func(ctx context.Context) {})
 
 	app.RegisterView(iris.HTML("./web/view", ".html"))
-	//注册控制器
-	mvc.New(app.Party("/hello")).Handle(new(controllers.MovieController))
 	_ = app.Run(iris.Addr(":8080"))
 }
